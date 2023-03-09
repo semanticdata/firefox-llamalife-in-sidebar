4dfa6c4fe33aa93922b6754ec71abc4127138794
Miguel Pimentel <108536517+datastring@users.noreply.github.com>
2023-03-09 09:35:19 -0600

    rewrite function

--- background.js
+++ background.js
@@ -9,17 +9,7 @@ function openPage() {
 browser.browserAction.onClicked.addListener(openPage);
 
 function openSidebar() {
-  sidebarAction.toggle();
+  browser.sidebarAction.toggle()
 }
 
 browser.browserAction.onClicked.addListener(openSidebar);
-
-// Update UI and set value of textbox
-async function updateUI() {
-  let commands = await browser.commands.getAll();
-  for (command of commands) {
-    if (command.name === sidebarToggle) {
-      document.querySelector('#shortcut').value = command.shortcut;
-    }
-  }
-}
