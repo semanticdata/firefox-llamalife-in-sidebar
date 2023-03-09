eaffe05f5d5990157d6b23627245529bc6ba72bd
Miguel Pimentel <108536517+datastring@users.noreply.github.com>
2023-03-09 09:53:42 -0600

    implemented new button in the toolbar

--- background.js
+++ background.js
@@ -1,12 +1,5 @@
 const sidebarToggle = '_execute_sidebar_action';
 
-// function openPage() {
-//   browser.tabs.create({
-//     url: "https://developer.mozilla.org",
-//   });
-// }
-// browser.browserAction.onClicked.addListener(openPage);
-
 // Update UI and set value of textbox
 async function updateUI() {
   let commands = await browser.commands.getAll();
@@ -17,6 +10,7 @@ async function updateUI() {
   }
 }
 
+// added by ~data
 function openSidebar() {
   browser.sidebarAction.toggle()
 }
