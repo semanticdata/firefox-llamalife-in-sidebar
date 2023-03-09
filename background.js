4b83e8e8a132fca2bdd038e0178fb73f2ba7d733
Miguel Pimentel <108536517+datastring@users.noreply.github.com>
2023-03-09 08:35:26 -0600

    fixed missing comma in script

--- background.js
+++ background.js
@@ -1,8 +1,7 @@
 function openPage() {
-    browser.tabs.create({
-      url: "https://developer.mozilla.org",
-    });
-  }
-  
-  browser.browserAction.onClicked.addListener(openPage);
-  
+  browser.tabs.create({
+    url: "https://developer.mozilla.org",
+  });
+}
+
+browser.browserAction.onClicked.addListener(openPage);
