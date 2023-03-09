8651078efacb8d65f5c646eecd6b945718fa76f3
Miguel Pimentel <108536517+datastring@users.noreply.github.com>
2023-03-09 09:50:41 -0600

    testing

--- background.js
+++ background.js
@@ -5,11 +5,22 @@ const sidebarToggle = '_execute_sidebar_action';
 //     url: "https://developer.mozilla.org",
 //   });
 // }
+// browser.browserAction.onClicked.addListener(openPage);
 
-browser.browserAction.onClicked.addListener(openPage);
+// Update UI and set value of textbox
+async function updateUI() {
+  let commands = await browser.commands.getAll();
+  for (command of commands) {
+    if (command.name === sidebarToggle) {
+      document.querySelector('#shortcut').value = command.shortcut;
+    }
+  }
+}
 
 function openSidebar() {
   browser.sidebarAction.toggle()
 }
 
 browser.browserAction.onClicked.addListener(openSidebar);
+
+document.addEventListener('DOMContentLoaded', updateUI);
